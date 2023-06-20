664576b91ab3958f22cd177272bfb2eadf11cfe9
AndyFaniry <ramarosonandyfaniry@gmail.com>
2023-06-20 20:33:14 +0300

    fix

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const verifyToken = require('../services/verifyToken');
+const verifyToken = require('../services/verifyToken.js');
 
 router.post('/login' , userController.login);
 router.post('/' , userController.createUser);
